3ed7e605300642b191eb7212ea909fa42ad41571
Антон <ComicAntonius@gmail.com>
2023-09-28 02:50:22 +0700

    Добавьте файлы проекта.

---
+++ pixel.cpp
@@ -0,0 +1,76 @@
+﻿#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    // Регистрация класса окна
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
+    wc.lpszClassName = L"GradientWindowClass";
+
+    RegisterClass(&wc);
+
+    // Создание окна
+    HWND hwnd = CreateWindow(L"GradientWindowClass", L"Градиент", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, screenWidth, screenHeight, NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    // Основной цикл сообщений
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        int screenWidth = ps.rcPaint.right - ps.rcPaint.left;
+        int screenHeight = ps.rcPaint.bottom - ps.rcPaint.top;
+
+        HDC memDC = CreateCompatibleDC(hdc);
+        HBITMAP memBitmap = CreateCompatibleBitmap(hdc, screenWidth, screenHeight);
+        HBITMAP hOldBitmap = (HBITMAP)SelectObject(memDC, memBitmap);
+
+        for (int y = 0; y < screenHeight; y++) {
+            for (int x = 0; x < screenWidth; x++) {
+                int red = x * 255 / screenWidth;
+                int green = y * 255 / screenHeight;
+                COLORREF color = RGB(red, green, 0);
+                SetPixel(memDC, x, y, color);
+            }
+        }
+
+        BitBlt(hdc, 0, 0, screenWidth, screenHeight, memDC, 0, 0, SRCCOPY);
+
+        SelectObject(memDC, hOldBitmap);
+        DeleteObject(memBitmap);
+        DeleteDC(memDC);
+
+        EndPaint(hwnd, &ps);
+        break;
+    }
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
